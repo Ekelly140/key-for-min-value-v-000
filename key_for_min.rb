f145efcee6a0ef1f33925f1eaf1ed35282090f42
ERIC KELLY <eakelly220@gmail.com>
2018-04-18 19:11:09 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   lowest = 0 
-  lowest_value = ""
+  lowest_value = nil
   name_hash.each do |name, value| 
   if lowest == 0 || lowest > value 
   lowest = value 
